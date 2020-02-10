b7f60c75a489be4c5f7050ec63e843fe56a6daaa
Dixon Whitmire <dwhitmire1@humana.com>
2020-02-09 22:51:45 -0500

    rough ui

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="header">
+    <h1>CRM App Logged In As {{ name }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data: function () {
+    return {
+      name: 'Dixon Whitmire'
+    }
+  }
+}
+</script>
---
+++ src/components/CustomerDetails.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="customer-details">
+    <label for="customer-select">Select a customer id:</label>
+    <select name="customers" id="customer-select">
+      <option value="">--Please select an option--</option>
+      <option value="all">All</option>
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'customer-details'
+}
+</script>
